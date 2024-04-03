6da82c7c33254cb114141d819a5fe5aeae9570b7
Chan Myae Oo <chanmyaeoodev@gmail.com>
2024-04-03 16:32:51 +0630

    user crud done

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Carbon;
 
@@ -58,38 +57,44 @@ class UserController extends Controller
     {
         //
     }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Http\Response
-     */
     public function edit(User $user)
     {
-        //
+        return view('admin.user.edit', compact('user'));
     }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, User $user)
     {
-        //
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users,id,' . $user->id],
+            'phone' => [
+                'required', 'string', 'max:255',
+                'unique:users,id,' . $user->id
+            ],
+            'role' => "required"
+        ]);
+
+        if ($request->hasFile('profile_picture')) {
+            $newName = uniqid() . '_profile_picture.' . $request->file("profile_picture")->getClientOriginalExtension();
+            $request->file("profile_picture")->storeAs("public", $newName);
+            $user->profile_picture = $newName;
+        } else {
+            $user->profile_picture = $request->old_profile_picture;
+        }
+
+        $user->name = $request->name;
+        $user->phone = $request->phone;
+        $user->email = $request->email;
+        $user->role = $request->role;
+        $user->email_verified_at = Carbon::now();
+        $user->password = Hash::make("password");
+        $user->update();
+        return redirect()->route('users.index')->with("message", "Successfully updated.");
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Http\Response
-     */
+
     public function destroy(User $user)
     {
-        //
+        $user->delete();
+        return redirect()->route('users.index')->with("message", "Successfully deleted.");
     }
 }
--- resources/views/admin/user/edit.blade.php
+++ resources/views/admin/user/edit.blade.php
@@ -2,51 +2,94 @@
 
 @section('content')
     <div class="my-4 d-flex align-items-center">
-        <a href="{{ route('books.index') }}" class="btn btn-sm btn-outline-dark me-2">
+        <a href="{{ route('users.index') }}" class="btn btn-sm btn-outline-dark me-2">
             <i class="bi bi-arrow-left"></i>
         </a>
         <h4 class="text-uppercase mb-0">
-            Edit Book
+            Member informatoin edit
         </h4>
     </div>
 
     <div class="card rounded-sm bg-white p-2">
         <div class="card-body">
-            <p class="fs-4 mb-3 fw-bold">Book Details</p>
-            <form action="{{ route('books.update', $book->id) }}" method="POST">
+            <p class="fs-4 mb-3 fw-bold">Member Informations</p>
+            <form action="{{ route('users.update', $user->id) }}" method="POST" enctype="multipart/form-data">
                 @csrf
                 @method('PUT')
-                <div class="mb-3">
-                    <label for="" class="mb-1">Book Title</label>
-                    <input type="text" name="title" class="form-control" value="{{ $book->title }}"
-                        placeholder="Enter Book Title...">
+
+                <div class="mb-3 mt-2">
+
+                    <div class="custom_photo_div d-inline-block">
+
+                        <img src="{{ asset('storage/' . $user->profile_picture) }}"
+                            class="w-100 shadow-sm rounded-3 shadow output">
+
+                        <div class="custom_photo_add_icon">
+                            <label for="image"
+                                class="photo_choose_icon btn btn-outline-dark image rounded shadow-sm w-100 text-center">
+                                <i class="bi bi-camera fs-5"></i>
+                            </label>
+                            <input type="hidden" value="{{ $user->profile_picture }}" name="old_profile_picture">
+                            <input type="file" id="image" name="profile_picture" accept="image/*"
+                                onchange="loadFile(event)" hidden>
+                        </div>
+                    </div>
                 </div>
 
                 <div class="mb-3">
-                    <label for="" class="mb-1">Author Name</label>
-                    <input type="text" name="author" class="form-control" value="{{ $book->author }}"
-                        placeholder="Enter Author Name...">
+                    <label for="" class="mb-1">Name</label>
+                    <input type="text" name="name" class="form-control @error('name') is-invalid @enderror"
+                        value="{{ $user->name }}" placeholder="Enter User Name...">
+                    @error('name')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
                 </div>
 
                 <div class="mb-3">
-                    <label for="" class="mb-1">Choose Book Category</label>
-                    <select class="form-select" aria-label="Default select example" name="category">
-                        @foreach ($categories as $category)
-                            <option value="{{ $category->id }}" {{ $book->category_id == $category->id ? 'selected' : '' }}>
-                                {{ $category->title }}
-                            </option>
-                        @endforeach
+                    <label for="" class="mb-1">Email</label>
+                    <input type="text" name="email" class="form-control @error('email') is-invalid @enderror"
+                        value="{{ $user->email }}" placeholder="Enter Member Email...">
+                    @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+
+                <div class="mb-3">
+                    <label for="" class="mb-1">Phone</label>
+                    <input type="text" name="phone" class="form-control @error('phone') is-invalid @enderror"
+                        value="{{ $user->phone }}" placeholder="Enter Member Phone...">
+
+                    @error('phone')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+
+                <div class="mb-3">
+                    <label for="" class="mb-1">Member Role</label>
+                    <select class="form-select" aria-label="Default select example" name="role">
+                        <option value="student" {{ $user->role == 'student' ? 'selected' : '' }}>Student</option>
+                        <option value="admin" {{ $user->role == 'admin' ? 'selected' : '' }}>Admin</option>
                     </select>
                 </div>
 
-                <div class="mb-3">
-                    <label for="" class="mb-1">Book Copies</label>
-                    <input type="number" name="qty" class="form-control" value="{{ $book->qty }}"
-                        placeholder="Enter Book Copies...">
-                </div>
-
-                <button class="btn lg_btn btn-primary float-end">Update Book</button>
+                <button class="btn lg_btn btn-primary float-end">Update</button>
             </form>
         </div>
     </div>
+    <script>
+        let loadFile = function(event) {
+            let output = document.querySelector(".output");
+            output.src = URL.createObjectURL(event.target.files[0]);
+            output.onload = function() {
+                URL.revokeObjectURL(output.src) // free memory
+                let error_msg = document.querySelector(".alert-danger").remove();
+            }
+        };
+    </script>
 @endsection
